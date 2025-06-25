35ea6dd52f27333700635b253cccb228ca8fa388
SAGAR SAGAR <sagarsagar81846@gmail.com>
2025-06-25 10:09:03 +0530

    error fix

--- src/Navication/Application.tsx
+++ src/Navication/Application.tsx
@@ -1,45 +1,55 @@
 import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import ExploreScreen from '../screen/Explore/explore'
-import Post from '../screen/Post/Post.js'
+
+import ExploreScreen from '../screen/Explore/explore';
+import Post from '../screen/Post/Post';
+import Chat from '../screen/Chat/chat';
+import Mange from '../screen/Mange/mange'
+ 
+// MainTabs ya ApplicationNavigator.js/tsx
+import MangeStack from './MangeStack';
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
 const MainTabs = () => (
-  <Tab.Navigator
+  <Tab.Navigator   
     screenOptions={({ route }) => ({
       tabBarIcon: ({ color, size }) => {
         const icons: Record<string, string> = {
           Explore: 'home',
-          Login: 'user', 
+          Post: 'plus',
+          Chat: 'envelope',
+          Mange: 'user',
         };
-        return <FontAwesome name={icons[route.name] || 'circle'} size={size} color={color} />;
-      },
+        return <FontAwesome name={icons[route.name] || 'circle'} size={33} color={color}  />;
+     },
       tabBarStyle: { backgroundColor: 'white' },
       tabBarActiveTintColor: 'tomato',
       tabBarInactiveTintColor: 'gray',
+  
       headerShown: true,
     })}
-  >
-    <Tab.Screen name="Explore" component={ExploreScreen} />
-    <Tab.Screen name="Post" component={Post} />
+   >
+    <Tab.Screen name="Explore" component={ExploreScreen} options={{ headerShown: false }} />
+    <Tab.Screen name="Post" component={Post} options={{ headerShown: false }} />
+    <Tab.Screen name="Chat" component={Chat} options={{ headerShown: false }} />
+    <Tab.Screen name="Mange" component={MangeStack} options={{ headerShown: false }} />
   </Tab.Navigator>
 );
 
-
-// Navigators
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-// Stack Navigator
 const ApplicationNavigators = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
       <Stack.Navigator initialRouteName="Main" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Main" component={MainTabs} />
-        <Stack.Screen name="Login" component={Post} options={{ headerShown: true }} />
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
